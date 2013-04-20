a203f6ab48aabe81e005ab906493ee713b501c61
Brian Mowrey <brian@drlabs.org>
2013-04-19 23:36:29 -0500

    initial version - My Tickets menu item added

--- init.rb
+++ init.rb
@@ -1,8 +1,17 @@
 Redmine::Plugin.register :redmine_my_tickets do
   name 'Redmine My Tickets plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Brian Mowrey'
+  description 'This is a plugin for redmine that adds a "My Tickets" link in the main menu'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/BrianMowrey/redmine-my-tickets'
+  author_url 'https://github.com/BrianMowrey'
+
+  menu :top_menu, :my_tickets, { :controller => 'issues', :assigned_to_id => 'me', :set_filter => 1 },{ :after => :my_page ,:if => Proc.new { User.current.logged? } }
+  
 end
+
+#TODO
+
+
+#add route
+#add settings to not add welcome route
